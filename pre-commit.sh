8f952abb5c6a27fc528c0e42e35c1a7fc9a9c7f5
Justice Amoh <justiceamoh@gmail.com>
2022-06-10 17:11:12 -0400

    Add more to pre-commit template

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,2 +1,20 @@
 #! /bin/bash
 VERSION=$(awk '/FIRMWARE/ { printf("%s.", $3); }' FirmwareVersion.h | sed 's/\./-/3;s/.$//;s/\"//g')
+
+commit_hash=$(git rev-parse --verify HEAD)
+
+if git diff --quiet HEAD^..HEAD FirmwareVersion.h
+then
+	echo ""
+else
+	# Initial commit: diff against an empty tree object
+	against=$(git hash-object -t tree /dev/null)
+fi
+
+
+if ! git diff --quiet HEAD^..HEAD FirmwareVersion.h
+then
+    VERSION=$(awk '/FIRMWARE/ { printf("%s.", $3); }' FirmwareVersion.h | sed 's/\./-/3;s/.$//;s/\"//g')
+    git tag -a "v$VERSION" -m "`git log -1 --format=%s`"
+    echo "FirmwareVersion.h changed. Created new tag, v$VERSION."
+fi
